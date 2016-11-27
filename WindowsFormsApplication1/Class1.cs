aaabab74c838bd702c35ec6d3c6d5efd886b00d1
Mickey <Mickey@MICKEY-PC>
2016-11-27 14:56:09 -0800

    new api

--- WindowsFormsApplication1/Class1.cs
+++ WindowsFormsApplication1/Class1.cs
@@ -6,9 +6,30 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication1
 {
-    class Class1
+    public class Class1
     {
-        //asdadsalsdhaldhlahdslasdh'
-        
+        public int id { get; set; }
+        public string Name { get; set; }
+        public List<string> Subject { get; set; }
+        public static List<Class1> GetAllStudets()
+        {
+            List<Class1> students = new List<Class1>
+            {
+            new Class1
+            {
+                id=1,
+                Name = "Mickey",
+                Subject = new List<string> {"C#","ASP","LINQ"}
+            },
+            new Class1
+            {
+                id=2,Name="Tinku", Subject= new List<string> {"DSA","ECE","SSS"}
+            },
+
+        };
+            return students;
+        }
+
     }
+   
 }
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+ 
 
 namespace WindowsFormsApplication1
 {
@@ -15,19 +16,40 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
-             int[] test = { -89,1, 25, 3, 4, 5, 6, 700, 8, 9, 10 };
-             
-            int max = 0;
-            for (int i = 0; i < test.Length; i++)
-            {
-                
-                if (test[i]>max)
-                {
-                    max = test[i];
-                }
-            }
-            int result = test.Aggregate((a,b)=>a+b);
+            // int[] test = { -89,1, 25, 3, 4, 5, 6, 700, 8, 9, 10 };
+            //var tess = test.Select((NUM, index) => new { Number = NUM, Index = index }).Select(a=>a.Number);
+            //int max = 0;
+            //foreach (var item in tess)
+            //{
+            //    Console.WriteLine(item );
+            //}
+            //for (int i = 0; i < test.Length; i++)
+            //{
 
+            //    if (test[i]>max)
+            //    {
+            //        max = test[i];
+            //    }
+            //}
+            //int result = test.Aggregate((a,b)=>a+b);
+            //TSQL2012Entities db = new TSQL2012Entities();
+            //List<Employee> emp = db.Employees.ToList();
+            //var dd = emp.Select((add,index)=> new { address= add.address,Index = index}).Where(c=>c.Index==1);
+            //var jiji = emp.Select(v => new { Address = v.address,Country = v.country});
+            //foreach (var item in jiji)
+            //{
+            //    Console.WriteLine(item.Address + "-3455345" + item.Country);
+            //}
+            IEnumerable<string> subjects = Class1.GetAllStudets().SelectMany(x => x.Subject);
+            
+            
+            //foreach (var item in subjects)
+            //{
+            //    Console.WriteLine(item.Name+"-"+item.StSubject);
+            //}
+            //Console.WriteLine(dd.Select(s=>s.address).FirstOrDefault());
+          
+            
         }
     }
 }
